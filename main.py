ac8c5fcb9e565ea85e002d27602c2817a5173d30
briancastro-bc <camilitooso77@gmail.com>
2021-08-18 13:10:36 -0500

    Código fuente: estructura base del aplicativo. BC

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.marks import *
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ src/marks.py
@@ -0,0 +1,12 @@
+
+class Marks:
+
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def calculate_average(marks: list):
+        pass
+
+    def marks_total_value() -> str:
+        pass
